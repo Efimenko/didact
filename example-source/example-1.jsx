d73e84808c7e0b69677bdc154a5323ef9d9423ae
Bohdan Yefimenko <Efimenko@users.noreply.github.com>
2024-07-04 19:43:04 +0300

    support text nodes

--- example-source/example-1.jsx
+++ example-source/example-1.jsx
@@ -1,3 +1,10 @@
 import { Didact } from "./src/index.js";
 
 const element = <div>first element</div>;
+
+const anotherElement = (
+  <div>
+    <span>some text</span>
+    <span>some more text</span>
+  </div>
+);
