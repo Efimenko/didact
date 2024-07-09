97cee50f28227d7844fcbb21256ccee72160e5d6
Bohdan Yefimenko <Efimenko@users.noreply.github.com>
2024-07-09 19:51:02 +0300

    create performUnitOfWork

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
+type PropsT = Record<string, unknown> & {
+  children: (ElementT | TextElementT)[];
+};
+
 type ElementT = {
   type: string;
-  props: Record<string, unknown> & { children: (ElementT | TextElementT)[] };
+  props: PropsT;
 };
 
 type TextElementT = {
@@ -9,6 +13,15 @@ type TextElementT = {
     nodeValue: string;
     children: [];
   };
+};
+
+type FiberT = {
+  type: string;
+  props: PropsT;
+  dom: undefined | HTMLElement | Text;
+  parent?: FiberT;
+  child?: FiberT;
+  sibling?: FiberT;
 };
 
 const createTextElement = (
@@ -41,16 +54,13 @@ const createElement = (
 
 const checkIsProp = (prop: string) => prop !== "children";
 
-const render = (
-  element: ElementT | TextElementT,
-  container: HTMLElement | Text,
-) => {
+const createDom = (fiber: ElementT | TextElementT) => {
   const dom =
-    element.type === "TEXT_ELEMENT"
+    fiber.type === "TEXT_ELEMENT"
       ? document.createTextNode("")
-      : document.createElement(element.type);
+      : document.createElement(fiber.type);
 
-  Object.keys(element.props)
+  Object.keys(fiber.props)
     .filter(checkIsProp)
     .forEach((prop) => {
       // TODO: add type guard for all possible html elements attributes
@@ -58,12 +68,91 @@ const render = (
       dom[prop] = element.props[prop];
     });
 
-  element.props.children.forEach((child) => {
-    render(child, dom);
-  });
-
-  container.appendChild(dom);
+  return dom;
 };
+
+const render = (element: ElementT | TextElementT, container: HTMLElement) => {
+  nextUnitOfWork = {
+    type: "ROOT",
+    dom: container,
+    props: {
+      children: [element],
+    },
+  };
+};
+
+let nextUnitOfWork: undefined | FiberT = undefined;
+
+const performUnitOfWork = (fiber: FiberT) => {
+  // add dom node
+  if (!fiber.dom) {
+    fiber.dom = createDom(fiber);
+  }
+
+  if (fiber.parent && fiber.parent.dom) {
+    fiber.parent.dom.appendChild(fiber.dom);
+  }
+
+  // create new fibers
+  const elements = fiber.props.children;
+
+  let index = 0;
+
+  let prevSibling: undefined | FiberT = undefined;
+
+  while (elements.length < index) {
+    const element = elements[index];
+
+    const newFiber = {
+      type: element.type,
+      props: element.props,
+      dom: undefined,
+      parent: fiber,
+    };
+
+    if (index === 0) {
+      fiber.child = newFiber;
+    } else {
+      if (!prevSibling) {
+        throw new Error("prevSibling should exist");
+      }
+
+      prevSibling.sibling = newFiber;
+    }
+
+    prevSibling = newFiber;
+
+    index++;
+  }
+
+  // return next unit of work
+  if (fiber.child) {
+    return fiber.child;
+  }
+
+  let nextFiber: undefined | FiberT = fiber;
+
+  while (nextFiber) {
+    if (nextFiber.sibling) {
+      return nextFiber.sibling;
+    }
+    nextFiber = nextFiber.parent;
+  }
+};
+
+const workLoop = (deadline: IdleDeadline) => {
+  let shouldYield = false;
+
+  while (nextUnitOfWork && !shouldYield) {
+    nextUnitOfWork = performUnitOfWork(nextUnitOfWork);
+
+    shouldYield = deadline.timeRemaining() < 1;
+  }
+
+  requestIdleCallback(workLoop);
+};
+
+requestIdleCallback(workLoop);
 
 export const Didact = {
   createElement,
